ba6d8be18bbf83b02d67f2a0a6821fafadde42de
ilsantos <ilsantos3@stefanini.com>
2023-11-04 11:39:40 -0300

    Iniciando projeto java com kafka

---
+++ src/main/java/io/kafka/demos/producers/ProducerDemo.java
@@ -0,0 +1,9 @@
+package io.kafka.demos.producers;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo lindo e maravilhoso!!");
+
+    }
+}
